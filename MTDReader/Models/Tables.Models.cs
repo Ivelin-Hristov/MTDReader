6beb2f7863027bcb9b89db300221337f2b484daf
Ivelin-Hristov <ivelinHRI@gmail.com>
2016-02-29 03:04:06 +0200

    Noter version - better classes. the parser works ok now.

--- MTDReader/Models/Tables.Models.cs
+++ MTDReader/Models/Tables.Models.cs
@@ -8,29 +8,22 @@
     using System.Threading.Tasks;
     using System.Xml;
     using System.IO;
-    public class TablesExport
+    public class Table
     {
-
-        public const string tableNameString = "Table";
-        public const string tableElementString = "Element";
 
         private string tableName;
         private string tableLabel;
 
         private List<string> sideAxis = new List<string>();
         private List<string> topAxis = new List<string>();
+
         private List<string> cellItemsInfo = new List<string>();
         private List<List<string>> cellMatrix = new List<List<string>>();
 
         private List<string> annotations = new List<string>();
         private bool colOrientation = true;
 
-        private XmlReader xmlDoc;
-        public XmlReader XmlDoc
-        {
-            get { return this.xmlDoc; }
-            set { this.xmlDoc = value; }
-        }
+        
 
         public string TableName
         {
@@ -106,6 +99,7 @@
                 this.cellMatrix = value;
             }
         }
+
         public List<string> Annotations
         {
             get
@@ -118,7 +112,6 @@
                 this.annotations = value;
             }
         }
-
         public bool ColOrientation
         {
             get
@@ -132,37 +125,138 @@
             }
         }
 
-        public TablesExport(string fileName)
+        public void Clear()
+        {
+            this.TableName = "";
+            this.TableLabel = "";
+            this.SideAxis.Clear();
+            this.TopAxis.Clear();
+            this.CellItemsInfo.Clear();
+            this.CellMatrix.Clear();
+       }
+
+    }
+
+    public class XMLParser
+    {
+
+        private bool writeToSideAxis = true;
+        private bool isInTables = false;
+
+        private XmlReader xmlDoc;
+
+        private Table table = new Table();
+
+        public XmlReader XmlDoc
+        {
+            get { return this.xmlDoc; }
+            set { this.xmlDoc = value; }
+        }
+
+        public void LoadMTD(string fileName)
         {
             this.xmlDoc = XmlReader.Create(fileName);
+         }
 
-        }
-        public void MoveToElement(string elementName, XmlNodeType elementType)
-        {
-
-            while (this.XmlDoc.Read())
-            {
-                if (this.XmlDoc.NodeType == elementType && this.XmlDoc.Name == elementName)
-                {
-                    break;
-                }
-                if (this.XmlDoc.EOF)
-                {
-                    break;                    
-                };
-            }
-
-        }
         public void Close()
         {
-            Console.WriteLine("XML Reader Diposed...");
             this.xmlDoc.Dispose();
 
         }
 
-        public void BuildTable()
+        private bool nodeEval(string nodeName, XmlNodeType nodeType, bool additionalExprssion = true)
         {
+            if (this.XmlDoc.Name == nodeName && this.XmlDoc.NodeType == nodeType && additionalExprssion)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
 
+        public Table MoveToNextTable()
+        {
+            this.table.Clear();
+            this.writeToSideAxis = true;
+            while (this.XmlDoc.Read())
+            {
+                //MOVE TO FIRST TABLE
+                if (this.nodeEval("Table",XmlNodeType.Element))
+                {
+                    this.isInTables = true;
+                }
+
+                //REMOVE AFTER LAST TABLE
+                if (this.nodeEval("Table",XmlNodeType.EndElement))
+                {
+                    this.isInTables = false;
+                }
+
+                //BREAK IF NOT TO FIRST
+                if (!this.isInTables)
+                {
+                    continue;
+                }
+
+                //TABLE
+                if (this.nodeEval("Table", XmlNodeType.Element))
+                {
+                    table.TableName = this.XmlDoc.GetAttribute("Name");
+                    table.TableLabel = this.XmlDoc.GetAttribute("Description");
+                }
+
+                if (this.nodeEval("Axis", XmlNodeType.Element, this.XmlDoc.GetAttribute("Name") == "Top"))
+                {
+                    this.writeToSideAxis = false;
+                }
+
+                //GOTO SIDE AXIS
+                if (this.nodeEval("Element", XmlNodeType.Element, this.writeToSideAxis))
+                {
+                    table.SideAxis.Add(this.XmlDoc.GetAttribute("Label"));
+                }
+
+                //GOTO TOP AXIS
+                if (this.nodeEval("Element", XmlNodeType.Element, !this.writeToSideAxis))
+                {
+                    table.TopAxis.Add(this.XmlDoc.GetAttribute("Label"));
+                }
+
+                //GOTO CELLITEMS
+                if (this.nodeEval("CellItem",XmlNodeType.Element))
+                {
+                    table.CellItemsInfo.Add(this.XmlDoc.GetAttribute("Type"));
+                }
+
+                //GOTO ROWS
+                if (this.nodeEval("row", XmlNodeType.Element))
+                {
+                    List<string> row = new List<string>();
+                    //CELL MATRIX [ROW,COL]
+                    for (var col = 1; col < this.XmlDoc.AttributeCount; col++)
+                    {
+                        row.Add(this.XmlDoc.GetAttribute(col));
+                    }
+                    table.CellMatrix.Add(row);
+                }
+
+                //GOTO ANNOTATIONS
+                if (this.nodeEval("Annotation", XmlNodeType.Element))
+                {
+                    table.Annotations.Add(this.XmlDoc.GetAttribute("Text"));
+                }
+
+                //RETURN TABLE (LEVEL IS THE LAST TAG IN TABLE)
+                if (this.nodeEval("Level", XmlNodeType.Element))
+                {
+                    break;
+                }     
+            }
+            return table;
         }
     }
+
 }
--- MTDReader/Program.cs
+++ MTDReader/Program.cs
@@ -16,115 +16,17 @@ namespace MTDReader
     {
         static void Main()
         {
-            //string mtdPath = @"C:\Users\BOX\Desktop\Global_Yearly_1415\CANADA_P&G_MALE GROOMING_ToplineData_Additional_Dec_2014_RE_CS.mtd";
-            string mtdPath = @"C:\Users\BOX\Desktop\Global_Yearly_1415\Global_Yearly_1415.mtd";
+            string mtdPath = @"C:\Users\BOX\Desktop\Global_Yearly_1415\CANADA_P&G_MALE GROOMING_ToplineData_Additional_Dec_2014_RE_CS.mtd";
+            //string mtdPath = @"C:\Users\BOX\Desktop\Global_Yearly_1415\Global_Yearly_1415.mtd";
 
-            var t = new TablesExport(mtdPath);
+            
+            var parser = new XMLParser();
+                parser.LoadMTD(mtdPath);
 
-            while (!t.XmlDoc.EOF)
-            {
-                //GOTO NEXT TABLE
-                t.MoveToElement(TablesExport.tableNameString, XmlNodeType.Element);
-                if (!t.XmlDoc.EOF)
-                {
-                    t.TableName = t.XmlDoc.GetAttribute("Name");
-                    t.TableLabel = t.XmlDoc.GetAttribute("Description");
-                }
+            var table = parser.MoveToNextTable();
+            var table2 = parser.MoveToNextTable();
 
-                //GOTO SIDE AXIS
-                while (t.XmlDoc.Read())
-                {
-                    if (t.XmlDoc.Name == "Axis" && t.XmlDoc.GetAttribute("Name") == "Top")
-                    {
-                        break;
-                    }
-
-                    if (t.XmlDoc.Name == "Element" && t.XmlDoc.NodeType == XmlNodeType.Element)
-                    {
-                        t.SideAxis.Add(t.XmlDoc.GetAttribute("Label"));
-                    }
-                    
-                }
-
-                //GOTO TOP AXIS 
-                while (t.XmlDoc.Read())
-                {
-                    if (t.XmlDoc.Name == "CellItems" && t.XmlDoc.NodeType == XmlNodeType.Element)
-                    {
-                        break;
-                    }
-
-                    if (t.XmlDoc.Name == "Element" && t.XmlDoc.NodeType == XmlNodeType.Element)
-                    {
-                        t.TopAxis.Add(t.XmlDoc.GetAttribute("Label"));
-                    }
-                }
-
-                //GOTO CELL ITEMS
-                while (t.XmlDoc.Read())
-                {
-                    if (t.XmlDoc.Name == "Layer" && t.XmlDoc.NodeType == XmlNodeType.Element)
-                    {
-                        break;
-                    }
-
-                    if (t.XmlDoc.Name == "CellItem" && t.XmlDoc.NodeType == XmlNodeType.Element)
-                    {
-                        t.CellItemsInfo.Add(t.XmlDoc.GetAttribute("Type"));
-                    }
-                }
-
-                //GOTO ROWS
-                while (t.XmlDoc.Read())
-                {
-                    if (t.XmlDoc.Name == "Layer" && t.XmlDoc.NodeType == XmlNodeType.EndElement)
-                    {
-                        break;
-                    }
-
-                    if (t.XmlDoc.Name == "row" && t.XmlDoc.NodeType == XmlNodeType.Element)
-                    {
-                        List<string> row = new List<string>();
-                        //CELL MATRIX [ROW,COL]
-                        for (var col = 1; col < t.XmlDoc.AttributeCount; col++)
-                        {
-                            row.Add(t.XmlDoc.GetAttribute(col));
-                        }
-                        t.CellMatrix.Add(row);
-
-                    }
-                }
-
-                //GOTO ANNOTATIONS
-                while (t.XmlDoc.Read())
-                {
-                    if (t.XmlDoc.Name == "Annotations" && t.XmlDoc.NodeType == XmlNodeType.EndElement)
-                    {
-                        break;
-                    }
-
-                    if (t.XmlDoc.Name == "Annotation" && t.XmlDoc.NodeType == XmlNodeType.Element)
-                    {
-                        t.Annotations.Add(t.XmlDoc.GetAttribute("Text"));
-                    }
-                }
-
-                Console.WriteLine(t.TableName);
-
-                t.TableName = "";
-                t.TableLabel = "";
-                t.SideAxis.Clear();
-                t.CellItemsInfo.Clear();
-                t.TopAxis.Clear();
-                t.CellMatrix.Clear();
-                t.Annotations.Clear();
-
-                //DEBUG BREAK - RUNS OLNY THE FIRST TABLE
-                //break;
-            }
-
-            t.Close();
-           
+            parser.Close(); 
         }
     }
 }
